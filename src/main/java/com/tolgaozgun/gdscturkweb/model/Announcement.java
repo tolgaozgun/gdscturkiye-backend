4b1f092f0d03e90ecc624372bb649059a70949a0
Tolga Özgün <tolgaozgunn@gmail.com>
2023-11-06 20:47:08 +0300

    Remove unused dependency

--- src/main/java/com/tolgaozgun/gdscturkweb/model/Announcement.java
+++ src/main/java/com/tolgaozgun/gdscturkweb/model/Announcement.java
@@ -4,7 +4,6 @@ import com.tolgaozgun.gdscturkweb.enums.UserType;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
